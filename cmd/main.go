b315c14c9741bbf1f97d0661c6f8c2453f94a249
mstgnz <genezmesut@gmail.com>
2024-02-19 14:17:36 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,16 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/favicon"
 	"github.com/joho/godotenv"
+	"github.com/minio/minio-go/v7"
 	"github.com/mstgnz/cdn/handler"
 	"github.com/mstgnz/cdn/service"
 )
 
 var (
-	awsService   = service.NewAwsService()
-	minioClient  = service.MinioClient()
-	imageHandler = handler.NewImage(minioClient, awsService)
-	awsHandler   = handler.NewAwsHandler(awsService)
+	awsService   service.AwsService
+	minioClient  *minio.Client
+	imageHandler handler.Image
+	awsHandler   handler.AwsHandler
 )
 
 func main() {
@@ -24,6 +25,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file, must be at project root")
 	}
+
+	awsService = service.NewAwsService()
+	minioClient = service.MinioClient()
+	imageHandler = handler.NewImage(minioClient, awsService)
+	awsHandler = handler.NewAwsHandler(awsService)
 
 	app := fiber.New(fiber.Config{
 		BodyLimit: 25 * 1024 * 2014,
